78ea2afa48d8f48bbd853449722bd97f77871191
shphung <stevenphunghuynh@yahoo.com>
2022-04-17 17:15:30 -0700

    Assignment3 Flyweight description
    Description of flyweight and justification

--- Assn 2 Prototype Pattern Server/PrivateServerAdvanced.java
+++
@@ -1,8 +0,0 @@
-public class PrivateServerAdvanced extends PrivateServer{
-    
-    public PrivateServerAdvanced(String privateServerName) {
-        this.serverName = privateServerName;
-
-        System.out.println(String.format("Advanced Private Server: %s created.", this.serverName));
-    }
-}
--- Assn 2 Prototype Pattern Server/PrivateServerBasic.java
+++
@@ -1,8 +0,0 @@
-public class PrivateServerBasic extends PrivateServer{
-    
-    public PrivateServerBasic(String privateServerName) {
-        this.serverName = privateServerName;
-
-        System.out.println(String.format("Basic Private Server: %s created.", this.serverName));
-    }
-}
